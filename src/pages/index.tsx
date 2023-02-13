8def00b7b139a4335cfc189aa3362dfdb03687d1
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-02-13 21:59:31 +0300

    fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/component/Banner";
 import Header from "@/component/Header";
+import Row from "@/component/Row";
 import requests from "@/utils/request";
 import Head from "next/head";
 import { Movie } from "typings";
@@ -60,7 +61,7 @@ export default function Home({
   documentaries,
 }: Props) {
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Netflix</title>
         <meta name="description" content="Netflix" />
@@ -71,11 +72,15 @@ export default function Home({
       <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16 ">
         <Banner netflixOriginals={netflixOriginals} />
         <section>
-          {/*row*/}
-          {/*row*/}
-          {/*row*/}
-          {/*row*/}
-          {/*row*/}
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
         </section>
         {/*modal*/}
       </main>
