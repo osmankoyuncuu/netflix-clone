fb963ff50d3e829968c4e80be29239cea619d8a9
Osman KOYUNCU <osmannnkoyuncuuu@gmail.com>
2023-02-22 23:31:46 +0300

    add stripe

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ["@stripe/firestore-stripe-payments"],
   images: {
     domains: ["image.tmdb.org", "assets.nflxext.com"],
   },
